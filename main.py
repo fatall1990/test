8fe7f415f3381544f4dc99a617f0e064e72f8324
fatall1990 <metadul@gmail.com>
2025-06-22 16:44:21 +0300

    добавлен клькулятор

--- main.py
+++ main.py
@@ -2,3 +2,13 @@ print("hello")
 print("изменил код добавил что то")
 print("тут работа с веткой")
 print("делаю изменения в коде")
+def add(a, b):
+    return a + b
+
+def subtract(a, b):
+    return a - b
+
+if __name__ == "__main__":
+    print("Калькулятор:")
+    print("2 + 3 =", add(2, 3))
+    print("5 - 1 =", subtract(5, 1))
